208adfb976759955b73b88a358763bb0d66362b0
vishal <vishal.kerai197@gmail.com>
2025-11-28 22:11:16 +0000

    new

---
+++ serve.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Quartz dev server with auto-restart on crash
+
+while true; do
+    echo "Starting Quartz server..."
+    npx quartz build --serve
+    exit_code=$?
+    echo "Server exited with code $exit_code. Restarting in 1 second..."
+    sleep 1
+done
